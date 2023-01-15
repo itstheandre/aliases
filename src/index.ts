cb9d598dd1baf0dade92bce66cfa3d01d0b46a8e
André de Albuquerque <itstheandre@gmail.com>
2023-01-15 11:32:35 +0100

    test

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { banana } from "@/app/banana";
+
+const PORT = 3000;
+
+const app = express();
+
+app.get("/test", (req, res) => {
+  res.status(200).send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`is banana? `, banana);
+});
